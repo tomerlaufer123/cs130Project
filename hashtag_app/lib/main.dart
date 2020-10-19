9bdd21c4ae709bf130474bbb05c4cb6bf2e57278
Takashi Joubert <tjoubert@g.ucla.edu>
2020-10-19 16:05:50 -0700

    Theme update, "search" photos on insta by tag

--- hashtag_app/lib/main.dart
+++ hashtag_app/lib/main.dart
@@ -12,7 +12,32 @@ void main() {
   runApp(MyApp());
 }
 
-// Widget displays hashtags generated, passed w/ tags constructor argument
+// Screen/Route displays hashtags generated
+// TODO: see if you could use instagram API Rio sent in the chat w/ tag
+class SimilarImages extends StatelessWidget {
+  @override
+  SimilarImages({this.tag});
+  final String tag; // the hashtag in question, passed as argument
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("Related to $tag")),
+        body: Scrollbar(
+            child: GridView.count(
+          crossAxisCount: 2,
+          children: List.generate(50, (index) {
+            return Container(
+              child: Card(
+                  color: const Color(0xff1a1a1a),
+                  child: Icon(Icons.image_outlined,
+                      color: Color(0xff737373), size: 60)),
+            );
+          }),
+        )));
+  }
+}
+
+// Screen/Route displays hashtags generated, passed w/ tags constructor argument
 class HashtagPage extends StatelessWidget {
   // make stateful? https://www.youtube.com/watch?v=PqeeMy1fQys&t=0s
   @override
@@ -25,25 +50,53 @@ class HashtagPage extends StatelessWidget {
         appBar: AppBar(
           title: Text("Your Hashtags: $total"),
         ),
-        // Generate new widget for every tag in list
+        // Show entry every hashtag in generated list
         body: Scrollbar(
           child: ListView.builder(
             itemCount: tags.length,
             itemBuilder: (context, index) {
-              return ListTile(
-                  title: Text('${tags[index]}'),
-                  //https://pub.dev/packages/clipboard_manager/example
-                  trailing: TextButton(
-                      child: Icon(Icons.content_copy),
-                      onPressed: () {
-                        ClipboardManager.copyToClipBoard("your text to copy")
-                            .then((result) {
-                          final snackBar = SnackBar(
-                            content: Text('Copied to Clipboard'),
-                          );
-                          Scaffold.of(context).showSnackBar(snackBar);
-                        });
-                      }));
+              return Card(
+                  margin: EdgeInsets.only(
+                      top: 4.0, bottom: 4.0, left: 9.0, right: 9.0),
+                  child: ListTile(
+                    // Every Entry has:
+                    // - hashtag
+                    // - Ctrl+C button
+                    // - button to view photos from inst with hashtag
+                    title: Text('${tags[index]}',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 19,
+                        )),
+                    trailing: Wrap(
+                      spacing: 0, // space between two icons
+                      children: <Widget>[
+                        TextButton(
+                            child: Icon(Icons.content_copy),
+                            onPressed: () {
+                              ClipboardManager.copyToClipBoard(
+                                      "your text to copy")
+                                  .then((result) {
+                                final snackBar = SnackBar(
+                                  content: Text('Copied to Clipboard'),
+                                );
+                                Scaffold.of(context).showSnackBar(snackBar);
+                              });
+                            }),
+                        /* TextButton( child: Icon(Icons.favorite), onPressed: null) */
+                        // maybe save liked hashtags
+                        TextButton(
+                            child: Icon(Icons.image_search),
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => SimilarImages(
+                                          tag: '${tags[index]}')));
+                            }),
+                      ],
+                    ),
+                  ));
             },
           ),
         ),
@@ -66,10 +119,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Hashtag Generator',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         primarySwatch: Colors.pink,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: const Color(0xFF575757),
       ),
       home: MyHomePage(title: 'Hashtag Generator'),
     );
@@ -91,10 +146,11 @@ class _MyHomePageState extends State<MyHomePage> {
   //state Object
   File _image; // Image file (saved in memory)
   List<String> _listOfTags; // File containing list of tags (just for testing)
-  final picker = ImagePicker(); //Plugin: https://pub.dev/packages/image_picker
+  final picker =
+      ImagePicker(); //Plugin: https://pub.dev/packages/image_picker/example
 
   // Get Image from Gallery
-  Future<void> _getImage(ImageSource source) async {
+  Future<void> _getImage(ImageSource source, {BuildContext context}) async {
     final pickedFile = await picker.getImage(
         source: source, maxWidth: 300.0, maxHeight: 500.0); //ImageSource.camera
 
@@ -110,7 +166,15 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  // Cropper plugin https://pub.dev/packages/image_cropper
+  // Reset Image
+  // DISCLAIMER pressing "choose from gallery" more than once still made app crash)
+  Future<void> _resetImage() async {
+    setState(() {
+      _image = null; // set image to captured photo
+    });
+  }
+
+  // Crop Image
   // Tutorial: https://fireship.io/lessons/flutter-file-uploads-cloud-storage/
   Future<void> _cropImage() async {
     File croppedImage = await ImageCropper.cropImage(
@@ -150,75 +214,123 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
 
-      body: ListView(
-        children: <Widget>[
-          if (_image != null) ...[
-            // Once photo selected:
-            // -  Display photo in center of screen
-            // -  Buttons to crop & redisplay
-            // -  Button to generate hashtags
-            Container(child: Image.file(_image)),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: _image != null
+          ? ListView(
+              // If photo has been selected:
+              // -  Display photo in center of screen
+              // -  Buttons to crop & redisplay
+              // -  Button to generate hashtags
               children: <Widget>[
-                ElevatedButton(
-                    //child: Icon(Icons.crop, color: Colors.white, size: 30),
-                    child: Text('Crop Image',
-                        style: TextStyle(fontSize: 18, color: Colors.white)),
-                    onPressed: _cropImage),
-                // TODO: Generate Hashtags Functionality to generate list_of_tags
-                ElevatedButton(
-                    child: Text('Generate Hashtags',
-                        style: TextStyle(fontSize: 18)),
-                    onPressed: _generateTags),
-                // change so list of tags is  file object after
-              ],
-            ),
-            // Once List of Hashtags generated:
-            // -  Button to view hashtags appears
-            // -  opens new page with displayed tags
-            Visibility(
-                visible: _listOfTags != null,
-                child: Center(
+                  Container(
+                      width: 50.0,
+                      height: 290.0,
+                      color: const Color(0xff1a1a1a),
+                      child: Image.file(_image)),
+                  Container(
+                      width: 50.0,
+                      height: 60.0,
+                      color: const Color(0xff1a1a1a),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            TextButton(
+                                child: Icon(Icons.crop,
+                                    color: Colors.white, size: 30),
+                                onPressed: _cropImage),
+                            TextButton(
+                              child: Icon(Icons.refresh,
+                                  color: Colors.white, size: 30),
+                              onPressed: _resetImage,
+                            )
+                          ])),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Padding(
+                          padding: EdgeInsets.only(top: 16.0, bottom: 16),
+                          child: ElevatedButton(
+                              // TODO: Implement Functionality to generate list_of_tags on backend
+                              child: Padding(
+                                padding: EdgeInsets.all(14.0),
+                                child: Text('Generate',
+                                    style: TextStyle(fontSize: 20)),
+                              ),
+                              onPressed: _generateTags)),
+                      // Once List of Hashtags generated:
+                      // -  Button to view hashtags appears
+                      // -  opens new page with displayed tags
+                      Padding(
+                        padding: EdgeInsets.only(top: 16.0, bottom: 16),
+                        child: Opacity(
+                            opacity: (_listOfTags != null) ? 1.0 : 0.4,
+                            child: ElevatedButton(
+                                style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          Color(0xfff76fb6)),
+                                ),
+                                child: Padding(
+                                  padding: EdgeInsets.all(14.0),
+                                  child: Text('View Hashtags',
+                                      style: TextStyle(fontSize: 20)),
+                                ),
+                                // When 'View Hashtags' Pressed, navigate to new page
+                                onPressed: () {
+                                  (_listOfTags != null)
+                                      ? Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => HashtagPage(
+                                                  tags: _listOfTags)))
+                                      : null;
+                                })),
+                      ),
+                    ],
+                  )
+                ])
+          : ListView(
+              // If photo not yet selected, options hidden
+              // - Show 'No Image Selected'
+              // - Greyed out Buttons
+              children: <Widget>[
+                  Container(
+                    width: 50.0,
+                    height: 300.0,
+                    color: const Color(0xff1a1a1a),
+                    //decoration: BoxDecoration(
+                    //    border: Border.all(color: Colors.white, width: 17)),
                     child: Padding(
-                  padding: EdgeInsets.all(30.0),
-                  child: ElevatedButton(
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Colors.blue),
-                      ),
-                      child: Padding(
-                        padding: EdgeInsets.all(16.0),
-                        child: Text('View Hashtags',
-                            style: TextStyle(fontSize: 25)),
-                      ),
-                      // When 'View Hashtags' Pressed, navigate to new page
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  HashtagPage(tags: _listOfTags)),
-                        );
-                      }),
-                )))
-          ] else
-            // If photo not yet selected, options hidden
-            Container(
-              width: 50.0,
-              height: 300.0,
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.white, width: 17)),
-              child: const DecoratedBox(
-                  decoration: const BoxDecoration(color: Colors.grey),
-                  child: Center(
-                      child: Text(
-                          'No image selected', //\n\nPress the camera button to pick an image!',
-                          style:
-                              TextStyle(fontSize: 17, color: Colors.white)))),
-            ),
-        ],
-      ),
+                      padding: const EdgeInsets.only(
+                          top: 16.0, left: 16.0, right: 16.0),
+                      child: const DecoratedBox(
+                          decoration: const BoxDecoration(
+                            color: const Color(0xff303030),
+                          ),
+                          child: Center(
+                              child: Text(
+                                  'No image selected', //\n\nPress the camera button to pick an image!',
+                                  style: TextStyle(
+                                      fontSize: 17, color: Colors.white)))),
+                    ),
+                  ),
+                  Container(
+                      width: 50.0,
+                      height: 60.0,
+                      color: const Color(0xff1a1a1a),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            TextButton(
+                                child: Icon(Icons.crop,
+                                    color: Color(0xff828282), size: 30),
+                                onPressed: null),
+                            TextButton(
+                              child: Icon(Icons.refresh,
+                                  color: Color(0xff828282), size: 30),
+                              onPressed: null,
+                            )
+                          ])),
+                ]),
 
       // Select image from camera or gallery by pressing button
       floatingActionButton: SpeedDial(
@@ -230,7 +342,7 @@ class _MyHomePageState extends State<MyHomePage> {
             label: 'Choose from Gallery',
             backgroundColor: Colors.blue,
             child: Icon(Icons.photo_library),
-            onTap: () => _getImage(ImageSource.gallery),
+            onTap: () => _getImage(ImageSource.gallery, context: context),
           ),
           SpeedDialChild(
             label: 'Take photo',

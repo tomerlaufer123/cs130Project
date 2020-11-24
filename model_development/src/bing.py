bb99b1541f3cc896dd2d569cae26bc1d43286d6a
rio <sonoyama.rio@gmail.com>
2020-11-24 23:21:05 +0900

    updated code related scraping

--- model_development/src/bing.py
+++ model_development/src/bing.py
@@ -8,32 +8,32 @@ Description: Class for web scrapping.
                 3. Remove unnecessary print statements
 '''
 from pathlib import Path
+import io
 import os
 import sys
-import urllib.request
-import urllib
-import imghdr
+# import urllib
 import posixpath
 import re
 import shutil
 from PIL import Image
 import requests
+import imghdr
 
 
 class Bing:
-    def __init__(self, query, limit, output_dir, adult, timeout, verbose, filters=''):
-        self.download_count = 0
+    def __init__(self, query, limit, output_dir, adult, timeout, verbose, offset):
+        self.d_count = 0
         self.query = query
         self.output_dir = output_dir
         self.adult = adult
-        self.filters = filters
-        self.downloaded_links = []
+        self.d_links = []
         self.limit = limit
         self.timeout = timeout
         self.verbose = verbose
+        self.offset = offset
 
-        self.headers = {'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0'}
-        self.page_counter = 0
+        self.headers = {'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:60.0) Gecko/20100101 Chrome/46.0.2490.80 Safari/537.36'}
+        self.p_count = 0
 
     def save_image(self, link, file_path):
         img = Image.open(requests.get(link, stream=True).raw)
@@ -49,43 +49,49 @@ class Bing:
         img.save(file_path)
 
     def download_image(self, link):
-        self.download_count += 1
-
+        self.d_count += 1
         # Get the image link
         try:
             file_path = os.path.join(
-                ".", self.output_dir, f"bing_{self.query}_{self.download_count}.jpg")
-            self.save_image(link, file_path) 
-        except Exception as e:
-            self.download_count -= 1
+                ".",
+                self.output_dir,
+                f"bing_{' '.join(self.query)}_{self.d_count}.jpg")
+            
+            self.save_image(link, file_path)
+        except Exception:
+            self.d_count -= 1
 
     def run(self):
         prev = 0
-        while self.download_count < self.limit:
-            try:
-                request_url = 'https://www.bing.com/images/async?q=' + urllib.parse.quote_plus(self.query) \
-                            + '&first=' + str(self.page_counter) + '&count=' + str(self.limit) \
-                            + '&adlt=' + self.adult + '&qft=' + self.filters
-                request = urllib.request.Request(request_url, None, headers=self.headers)
-                response = urllib.request.urlopen(request)
-                html = response.read().decode('utf8')
-                links = re.findall('murl&quot;:&quot;(.*?)&quot;', html)
+        while self.d_count < self.limit:
+            params = {
+                "q": '+'.join(self.query),
+                "first": self.p_count*self.offset,
+                "count": self.offset,
+                "adlt": "off"}
+                # "mkt": "en-us"}
 
-                for link in links:
-                    # Avoid downloading duplicate images
-                    if link in self.downloaded_links:
-                        continue
-                    elif self.download_count < self.limit:
-                        self.download_image(link)
-                        self.downloaded_links.append(link)
-                        if self.verbose \
-                                and self.download_count % (self.limit//10) == 0 \
-                                and prev != self.download_count:
-                            print(f"Downloaded {self.download_count}/{self.limit} images")
-                            prev = self.download_count
-                    else:
-                        break
-            except:
-                pass
+            response = requests.get(
+                "https://www.bing.com/images/async",
+                headers=self.headers,
+                params=params)
 
-            self.page_counter += 1
+            links_ = re.findall('murl&quot;:&quot;(.*?)&quot;', response.text)
+
+            # Avoid downloading duplicate images
+            links = [link for link in links_ if link not in self.d_links]
+            self.d_links += links
+
+            for link in links:
+                # print(link)
+                if self.d_count < self.limit:
+                    self.download_image(link)
+                    if self.verbose \
+                            and self.d_count % (self.limit//10) == 0 \
+                            and prev != self.d_count:
+                        print(f"Downloaded {self.d_count}/{self.limit} images")
+                        prev = self.d_count
+                else:
+                    break
+
+            self.p_count += 1
--- model_development/src/download_images.py
+++ model_development/src/download_images.py
@@ -1,7 +1,6 @@
 '''
 File: download_images.py
 Description: This is used to download additional images.
-             `download` is copied from https://github.com/gurugaurav/bing_image_downloader.
              See `notebooks/6-additional-images.ipynb` for related info.
 Usage: python download_images.py add_count.csv /dir/to/save/images
 '''
@@ -10,45 +9,11 @@ import pickle
 import os
 import sys
 from math import ceil
-
+from random import uniform
+import time
 import pandas as pd
 import numpy as np
-from random import seed, randint
-
-from bing import Bing
-
-
-def download(
-        query,
-        limit=100,
-        output_dir="bing_images",
-        adult_filter_off=True,
-        force_replace=True,
-        timeout=5,
-        verbose=False):
-
-    # engine = 'bing'
-    if adult_filter_off:
-        adult = 'off'
-    else:
-        adult = 'on'
-
-    cwd = os.getcwd()
-    image_dir = os.path.join(cwd, output_dir)
-
-    if force_replace:
-        if os.path.isdir(image_dir):
-            shutil.rmtree(image_dir)
-
-    # check directory and create if necessary
-    try:
-        if not os.path.isdir("{}/{}/".format(cwd, output_dir)):
-            os.makedirs("{}/{}/".format(cwd, output_dir))
-    except:
-        pass
-
-    bing = Bing(query, limit, output_dir, adult, timeout, verbose)
-    bing.run()
+from google_crawler import Google
 
 
 def main():
@@ -61,45 +26,69 @@ def main():
     filename = sys.argv[1]
     dest_dir = sys.argv[2]
 
+    # check directory and create if necessary
+    cwd = os.getcwd()
+    if not os.path.isdir("{}/{}/".format(cwd, dest_dir)):
+        os.makedirs("{}/{}/".format(cwd, dest_dir))
+
     # Read sim_tag dictionary
-    with open("./model/sim_tag.pickle", "rb") as f:
+    with open("misc/sim_tag.pickle", "rb") as f:
         sim_tag = pickle.load(f)
 
+    # Read log
+    with open("log/download_query_log") as f:
+        completed_quries = f.read().split('\n')
     df = pd.read_csv(filename, index_col=0, names=["count"])
+
     add_count = df["count"]
     total = int(add_count.sum())
     to_be_added = int(add_count.sum())
     curr_added = 0
 
-    seed(42)
     while True:
-        idx = randint(0, add_count.shape[0])
+        # All queires completed
+        if len(set(add_count.index).difference(set(completed_quries))) == 0:
+            print("[=] All queries completed")
+            exit(0)
+
+        idx = int(time.time()) % add_count.shape[0]
         base = add_count.index[idx]
         sim_tags = [base] + [tag for tag in sim_tag[base] if tag in add_count.index]
         num_tags = len(sim_tags)
         limit = ceil(add_count[base] / num_tags)
 
         for i in range(num_tags):
-            query = ' '.join(sim_tags[:i+1])
-            download(
-                query,
-                output_dir=dest_dir,
-                limit=limit,
-                force_replace=False)
+            query = sim_tags[:i+1]
+            if query in completed_quries:
+                continue
+            # q_tag = ['#'+q for q in query]
+            # print(f"[ ] Collecting {limit} image srouces for: {' '.join(q_tag)}")
+            d_count, reached_end = Google(query, limit, dest_dir).run()
+            # print(f"[+] Downloaded {d_count} images for: {' '.join(q_tag)}")
 
-            add_count[sim_tags[i]] \
-                = max(0, add_count[sim_tags[i]] - limit*(num_tags-i))
-            print(f"Downloaded {limit} images of '{query}'")
+            if reached_end:
+                completed_quries.append(' '.join(query))
+
+            for tag in query:
+                add_count[tag] = max(0, add_count[tag] - d_count)
+            t = uniform(1, 2)
+            time.sleep(t)
 
         temp = to_be_added
         add_count = add_count.where(add_count > 0).dropna()
         to_be_added = int(add_count.sum())
         curr_added += temp - to_be_added
         print(f"[{curr_added}/{total}]")
-        if to_be_added <= 0:
-            break
 
-        add_count.to_csv("download_images_log.csv", header=False)
+        # Log
+        add_count.to_csv(filename, header=False)
+        if reached_end:
+            with open("log/download_query_log", "w") as f:
+                f.writelines("%s\n" % query for query in completed_quries)
+
+        if to_be_added <= 0:
+            print("[=] All queries completed")
+            exit(0)
 
 
 if __name__ == "__main__":
